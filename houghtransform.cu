64621c6250433871951764fbcd238c702e488477
AndiH <a.herten+github@gmail.com>
2012-07-23 10:26:16 +0200

    Changed path to cuPrintf

--- houghtransform.cu
+++ houghtransform.cu
@@ -12,7 +12,7 @@
 #include <thrust/iterator/zip_iterator.h>
 #include <thrust/sequence.h>
 
-#include "../../CUDA/local/cuPrintf.cu"
+#include "/cuPrintf.cu"
 
 struct confMap {
 	__device__ thrust::tuple<double, double> operator() (thrust::tuple<double, double> &data) {

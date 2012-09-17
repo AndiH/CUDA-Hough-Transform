63e552314cbc27f95f85e74785486eedfc0838fa
Andreas Herten <a.herten@fz-juelich.de>
2012-09-17 16:24:14 +0200

    Fixed wrong binning of doubles - now using floats

--- AhTranslatorFunction.h
+++ AhTranslatorFunction.h
@@ -18,9 +18,9 @@ public:
 	
 	__device__ int operator() (const double &value) const {
 		float result = ((value - firstValue)/widthOfCell);
-		if (result > 31100 && result < 31200){
-			printf("DEBUG - result = %.8f = (%.8f - %.8f)/%.8f\n", result, value, firstValue, widthOfCell);
-		}
+//		if (result > 31100 && result < 31200){
+//			printf("DEBUG - result = %.8f = (%.8f - %.8f)/%.8f\n", result, value, firstValue, widthOfCell);
+//		}
 		return (int)result; //casting a double to an int doesnt work, with float it does, hooray
 	}
 

996d18a84063cd391887a06792b8c896be3dd49f
Andreas Herten <a.herten@fz-juelich.de>
2012-10-31 10:49:09 +0100

    Initial commit of class-transferred Hough Transformation

--- AhHoughTransformation.cu
+++ AhHoughTransformation.cu
@@ -3,12 +3,16 @@
 AhHoughTransformation::AhHoughTransformation()
 {}
 
-AhHoughTransformation::AhHoughTransformation(thrust::host_vector<double> xValues, thrust::host_vector<double> yValues)
+AhHoughTransformation::AhHoughTransformation(thrust::host_vector<double> xValues, thrust::host_vector<double> yValues, double maxAngle, double everyXDegrees)
 : fXValues(xValues),
-  fYValues(yValues)
+  fYValues(yValues),
+  fMaxAngle(maxAngle),
+  fEveryXDegrees(everyXDegrees)
+
 {
 	DoChangeContainerToTwoTuples();
 	DoConformalMapping();
+	DoGenerateAngles();
 	DoHoughTransform();
 }
 
@@ -16,7 +20,7 @@ AhHoughTransformation::~AhHoughTransformation()
 {}
 
 AhHoughTransformation::DoChangeContainerToTwoTuples() {
-	//! change container from vec<x> and vec<y> to vec<tuple<x, y> >
+	//!< change container from vec<x> and vec<y> to vec<tuple<x, y> >
 	thrust::copy(
 		thrust::make_zip_iterator(
 			fXValues.begin(), 
@@ -31,11 +35,59 @@ AhHoughTransformation::DoChangeContainerToTwoTuples() {
 }
 
 AhHoughTransformation::DoConformalMapping() {
-	//! conformal mapping
+	//!< conformal mapping
 	thrust::transform(
 		fXYValues.begin(),
 		fXYValues.end(),
-		fXYValues.begin(), // in place copy - maybe not so full of sense? think about it
+		fXYValues.begin(), // in place copy (== output vector = input vector) - maybe not so full of sense? think about it
 		my::confMap()
 	);
+}
+
+AhHoughTransformation::DoGenerateAngles() {
+	//!< Resize angle vector to match the actual size
+	fAngles.resize(fMaxAngle/fEveryXDegrees);
+	//!< Fill it
+	thrust::sequence(
+		fAngles.begin(), 
+		fAngles.end(), 
+		0., 
+		fEveryXDegrees
+	);
+}
+
+AhHoughTransformation::DoHoughTransform() {
+	/** Attention
+	* For every (x*,y*) point a hough transform is done!
+	* While this hough transform itself is done in parallel, all hough transforms in whole are done serial, one by one after an other.
+	* This might be a point of huge improvements
+	*/
+	for (int iDataPoints = 0; iDataPoints < fXYValues.size(); iDataPoints++) {
+		thrust::device_vector<double> d_tempData(fAngles.size()); //!< Temp vector which is being filled and then pushed back to the main return vector. For every angle point theres a data point, so that's the size of it
+
+		thrust::constant_iterator<thrust::tuple<double, double> > currentData(fXYValues[iDataPoints]); //!< create constant iterator for the conf mapped data 2-tuples
+
+		//! following transformation uses the operator of htransf to run over all elements
+		//!   elements being a iterator from angles.start to angles.end with each time the constant iterator with the conf mapped 2-tuple
+		//!   the result of the calculation is written in to the d_tempData vector
+		thrust::transform(
+			thrust::make_zip_iterator(
+				thrust::make_tuple(
+					fAngles.begin(),
+					currentData
+				)
+			),
+			thrust::make_zip_iterator(
+				thrust::make_tuple(
+					fAngles.end(),
+					currentData
+				)
+			),
+			d_tempData.begin(),
+			my::htransf()
+		);
+
+		fTransformedPoints.push_back(d_tempData); //!< push it back to the main data stack vector
+
+	}
 }
--- AhHoughTransformation.h
+++ AhHoughTransformation.h
@@ -27,23 +27,67 @@ namespace my { // I don't know if this is a good idea are bad coding style
 			return thrust::make_tuple(x/x2y2, -y/x2y2);
 		}
 	};
+
+	/**
+	* @brief (Outsourced) Function which actually does the hough transformation
+	* @param alpha Angle to be hough transformed with, in degrees
+	* @param x,y Coordinates of the to be transformed point
+	* @return The hough transformed point corresponding to a certain angle (as a double)
+	* 
+	* This actual hough transforming method has been outsourced in order to call different hough transform functions from the operator mother function
+	*/
+	__device__ double houghTransfFunction(double alpha, double x, double y) {
+		return (cos(alpha*1.74532925199432955e-02) * x + sin(alpha*1.74532925199432955e-02) * y);
+	}
+
+	/**
+	* @brief Struct for an operator which hough transforms its input due to an exchangeable hough transforming method
+	* @param data A tuple of (an angle and a tuple of (to be hough transformed x and y coordinates))
+	* @return A hough transformed point corresponding to a certain angle
+	* 
+	* This method only runs on the device
+	*/
+	struct htransf {
+		__device__ double operator() (thrust::tuple<double, thrust::tuple<double, double> > data) {
+			double alpha = thrust::get<0>(data);
+			thrust::tuple<double, double> xy = thrust::get<1>(data);
+			double x = thrust::get<0>(xy);	
+
+			double y = thrust::get<1>(xy);
+
+	// 		printf("alpha * x + alpha * y = %f * %f + %f * %f\n", alpha, x, alpha, y);
+
+			return houghTransfFunction(alpha, x, y);
+		}
+	};
 }
 
 class AhHoughTransformation
 {
 public:
 	AhHoughTransformation();
-	AhHoughTransformation(thrust::host_vector<double>, thrust::host_vector<double>);
+	AhHoughTransformation(thrust::host_vector<double>, thrust::host_vector<double>, double maxAngle = 180., double everyXDegrees = 30.);
 	
 	virtual ~AhHoughTransformation();
+
+	thrust::device_vector<double> GetAngles() { return fAngles; };
+	std::vector<thrust::device_vector<double> > GetVectorOfTransformedPoints() {return fTransformedPoints; };
 protected:
 	DoChangeContainerToTwoTuples();
 	DoConformalMapping();
+	DoGenerateAngles();
 	DoHoughTransform();
 private:
+	// data containers
 	thrust::device_vector<double> fXValues;
 	thrust::device_vector<double> fYValues;
 	thrust::device_vector<thrust::tuple<double, double> > fXYValues;
+	thrust::device_vector<double> fAngles;
+	std::vector<thrust::device_vector<double> > fTransformedPoints;
+
+	// needed values
+	double fMaxAngle; // (right border of) range of the angles to investigate, [0, maxAngle]
+	double fEveryXDegrees; // make a data point every x degrees
 
 };
 #endif //AHHOUGHTRANSFORMATION_H

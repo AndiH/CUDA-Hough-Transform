c8e22e00d983f7ed365d789be539e36ef8dbc5d7
Andreas Herten <a.herten@fz-juelich.de>
2012-11-15 15:52:18 +0100

    Added method for reading from input file

--- houghtransform.cu
+++ houghtransform.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <algorithm> // to find minimum --> min_element
 #include "stdio.h"
 #include <vector>
@@ -48,6 +49,30 @@
  **/
 
 /**
+ * @brief Reads data from specific formated file into three given vectors
+ * @param filename       Filename (include path if necessary)
+ * @param x              x-value vector (1st row of file)
+ * @param y              y-value vector (2nd row of file)
+ * @param r              r-value vector (4th row of file)
+ * @param upToLineNumber Up to with line (including that line) the file will be read. Starts at line 1.
+ *
+ * Uses just x, y and r at the moment, because that's all I need.
+ */
+void readPoints(std::string filename, std::vector<double> &x, std::vector<double> &y, std::vector<double> &r, int upToLineNumber = 2) {
+	std::ifstream file(filename.c_str());
+	float tempX, tempY, tempZ, tempR, tempI;
+	char tempChar[10];
+	int i = 1;
+	while(i <= upToLineNumber && file >> tempX >> tempY >> tempZ >> tempR >> tempI >> tempChar) {
+		x.push_back(tempX);
+		y.push_back(tempY);
+		r.push_back(tempR);
+		i++;
+	}
+	file.close();
+}
+
+/**
  * @brief Helper function which simply prints out the contents of a tuple
  * @param thatTuple A tuple of doubles to be printed
  * @return Nothing, it's a void.
@@ -75,86 +100,10 @@ int main (int argc, char** argv) {
 	//! fill original data
 	std::vector<double> x;
 	std::vector<double> y;
-//	x.push_back(-3.89038); y.push_back(-3.14085);
-//	x.push_back(-8.42652); y.push_back(-5.38459);
-//	x.push_back(-13.4702); y.push_back(-6.59945);
-//	x.push_back(-18.8446); y.push_back(-6.69932);
-//	x.push_back(-24.3888); y.push_back(-5.49439);
-//	x.push_back(-29.8641); y.push_back(-2.8527);
+	std::vector<double> r;
 
-	x.push_back(-0.287274); y.push_back(-1.668553); // MVD POINT EVENT 1
-	x.push_back(-0.378530); y.push_back(-2.244961); // MVD POINT EVENT 1
-	x.push_back(-0.557000); y.push_back(-3.422990); // MVD POINT EVENT 1
-// 	x.push_back(-0.763607); y.push_back(-4.893884); // MVD POINT EVENT 1
-// 	x.push_back(1.3155927); y.push_back(-1.276497); // MVD POINT EVENT 1
-// 	x.push_back(1.7415385); y.push_back(-1.742629); // MVD POINT EVENT 1
-// 	x.push_back(2.5251355); y.push_back(-2.654892); // MVD POINT EVENT 1
-// 	x.push_back(3.6173491); y.push_back(-4.050041); // MVD POINT EVENT 1
+	readPoints("data.dat", x, y, r, 18);
 
-// 	x.push_back(8.6005649); y.push_back(-14.18077); // STT POINT EVENT 2
-// 	x.push_back(8.8446016); y.push_back(-15.12664); // STT POINT EVENT 2
-// 	x.push_back(9.0629711); y.push_back(-16.09391); // STT POINT EVENT 2
-// 	x.push_back(9.1932706); y.push_back(-16.74574); // STT POINT EVENT 2
-// 	x.push_back(9.3548145); y.push_back(-17.71571); // STT POINT EVENT 2
-// 	x.push_back(9.4836196); y.push_back(-18.70058); // STT POINT EVENT 2
-// 	x.push_back(9.5774650); y.push_back(-19.69842); // STT POINT EVENT 2
-// 	x.push_back(9.6384601); y.push_back(-20.70427); // STT POINT EVENT 2
-// 	x.push_back(9.6667957); y.push_back(-21.91839); // STT POINT EVENT 2
-// 	x.push_back(9.6510515); y.push_back(-23.02375); // STT POINT EVENT 2
-
-
-// 	x.push_back(-1.687104); y.push_back(-1.412673); // MVD POINT EVENT 2
-// 	x.push_back(-3.564506); y.push_back(-3.116787); // MVD POINT EVENT 2
-// 	x.push_back(-6.729347); y.push_back(-6.360077); // MVD POINT EVENT 2
-// 	x.push_back(-8.854902); y.push_back(-8.857123); // MVD POINT EVENT 2
-// 	x.push_back(3.6133863); y.push_back(3.8080084); // MVD POINT EVENT 2
-// 	x.push_back(6.5745592); y.push_back(6.6399340); // MVD POINT EVENT 2
-// 	x.push_back(8.9718790); y.push_back(8.7770128); // MVD POINT EVENT 2
-// 	x.push_back(6.5918564); y.push_back(6.6557998); // MVD POINT EVENT 2
-// 	x.push_back(6.3520546); y.push_back(7.0406150); // MVD POINT EVENT 2
-// //
-// 	x.push_back(-11.55082); y.push_back(-12.50229); // STT POINTS EVENT 2
-// 	x.push_back(-12.05648); y.push_back(-13.25516); // STT POINTS EVENT 2
-// 	x.push_back(-12.51873); y.push_back(-13.96542); // STT POINTS EVENT 2
-// 	x.push_back(-12.99433); y.push_back(-14.72306); // STT POINTS EVENT 2
-// 	x.push_back(-13.44450); y.push_back(-15.46083); // STT POINTS EVENT 2
-// 	x.push_back(-13.89046); y.push_back(-16.22060); // STT POINTS EVENT 2
-// 	x.push_back(-14.32617); y.push_back(-16.98514); // STT POINTS EVENT 2
-// 	x.push_back(-14.74108); y.push_back(-17.74436); // STT POINTS EVENT 2
-// 	x.push_back(-15.21368); y.push_back(-18.64175); // STT POINTS EVENT 2
-// 	x.push_back(-15.62225); y.push_back(-19.44583); // STT POINTS EVENT 2
-// 	x.push_back(-16.17100); y.push_back(-20.58612); // STT POINTS EVENT 2
-// 	x.push_back(-16.51310); y.push_back(-21.33209); // STT POINTS EVENT 2
-// 	x.push_back(-17.03618); y.push_back(-22.52636); // STT POINTS EVENT 2
-// 	x.push_back(-17.34774); y.push_back(-23.26947); // STT POINTS EVENT 2
-// 	x.push_back(-17.85295); y.push_back(-24.55199); // STT POINTS EVENT 2
-// 	x.push_back(-18.12363); y.push_back(-25.27700); // STT POINTS EVENT 2
-// 	x.push_back(-18.61602); y.push_back(-26.68180); // STT POINTS EVENT 2
-// 	x.push_back(-18.85255); y.push_back(-27.39675); // STT POINTS EVENT 2
-// 	x.push_back(-19.13898); y.push_back(-28.31199); // STT POINTS EVENT 2
-// 	x.push_back(-19.41473); y.push_back(-29.24073); // STT POINTS EVENT 2
-// 	x.push_back(12.852836); y.push_back(11.957662); // STT POINTS EVENT 2
-// 	x.push_back(13.342592); y.push_back(12.337225); // STT POINTS EVENT 2
-// 	x.push_back(14.137040); y.push_back(12.941991); // STT POINTS EVENT 2
-// 	x.push_back(14.940003); y.push_back(13.539941); // STT POINTS EVENT 2
-// 	x.push_back(15.750968); y.push_back(14.130979); // STT POINTS EVENT 2
-// 	x.push_back(16.570022); y.push_back(14.714738); // STT POINTS EVENT 2
-// 	x.push_back(17.396743); y.push_back(15.290876); // STT POINTS EVENT 2
-// 	x.push_back(18.230621); y.push_back(15.859502); // STT POINTS EVENT 2
-// 	x.push_back(19.105146); y.push_back(16.441886); // STT POINTS EVENT 2
-// 	x.push_back(19.955823); y.push_back(16.995523); // STT POINTS EVENT 2
-// 	x.push_back(20.869682); y.push_back(17.577196); // STT POINTS EVENT 2
-// 	x.push_back(21.278230); y.push_back(17.831554); // STT POINTS EVENT 2
-// 	x.push_back(21.718780); y.push_back(18.102874); // STT POINTS EVENT 2
-// 	x.push_back(22.138130); y.push_back(18.357793); // STT POINTS EVENT 2
-// 	x.push_back(22.929496); y.push_back(18.831468); // STT POINTS EVENT 2
-// 	x.push_back(23.338378); y.push_back(19.071701); // STT POINTS EVENT 2
-// 	x.push_back(23.803163); y.push_back(19.342851); // STT POINTS EVENT 2
-// 	x.push_back(25.082901); y.push_back(20.070076); // STT POINTS EVENT 2
-// 	x.push_back(25.542038); y.push_back(20.325212); // STT POINTS EVENT 2
-// 	x.push_back(26.426181); y.push_back(20.807189); // STT POINTS EVENT 2
-// 	x.push_back(27.338024); y.push_back(21.292156); // STT POINTS EVENT 2
-// 	x.push_back(27.818302); y.push_back(21.541893); // STT POINTS EVENT 2
 
 	//! Preparations
 	thrust::host_vector<double> h_x = x;
